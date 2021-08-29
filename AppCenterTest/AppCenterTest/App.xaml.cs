d2d22a9a4dc26e2b359ad0d44df87a62864b9403
Natalya Smirnova <natalya.smirnova@akvelon.com>
2021-08-29 22:51:19 +0300

    repo init

---
+++ AppCenterTest/AppCenterTest/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppCenterTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("ios=f9b3614c-6afe-4c58-9b71-31ab1397de11;" +
+                  "uwp=5718fb7e-563f-4e6b-89ce-5b3785695335;" +
+                  "android=7159fddf-b0a6-42fe-b903-8358e0580338",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
