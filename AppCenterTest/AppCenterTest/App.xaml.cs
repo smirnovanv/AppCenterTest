727801820c766ef17df2c6193ddf975227c62d05
Natalya Smirnova <natalya.smirnova@akvelon.com>
2021-08-30 06:23:23 +0300

    remove ios

--- AppCenterTest/AppCenterTest/App.xaml.cs
+++ AppCenterTest/AppCenterTest/App.xaml.cs
@@ -18,9 +18,8 @@ namespace AppCenterTest
 
         protected override void OnStart()
         {
-            AppCenter.Start("ios=f9b3614c-6afe-4c58-9b71-31ab1397de11;" +
-                  "uwp=5718fb7e-563f-4e6b-89ce-5b3785695335;" +
-                  "android=7159fddf-b0a6-42fe-b903-8358e0580338",
+            AppCenter.Start("android=7159fddf-b0a6-42fe-b903-8358e0580338;" +
+                  "uwp=5718fb7e-563f-4e6b-89ce-5b3785695335",
                   typeof(Analytics), typeof(Crashes));
         }
 
