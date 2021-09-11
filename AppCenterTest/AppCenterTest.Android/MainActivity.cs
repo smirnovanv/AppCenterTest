58f17c2ea5f9f642e4122c842287544df8134241
Natalya Smirnova <natalya.smirnova@akvelon.com>
2021-09-11 13:20:34 +0300

    Test android

--- AppCenterTest/AppCenterTest.Android/MainActivity.cs
+++ AppCenterTest/AppCenterTest.Android/MainActivity.cs
@@ -5,6 +5,8 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 
+//add test
+
 namespace AppCenterTest.Droid
 {
     [Activity(Label = "AppCenterTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
